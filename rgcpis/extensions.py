8a9df2b992264d493b9ccff63838145b284d1309
gongxiuj <gong.xiujin@renderg.com>
2016-08-09 16:03:29 +0800

    modify manage.py

--- rgcpis/extensions.py
+++ rgcpis/extensions.py
@@ -1,11 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_bootstrap import Bootstrap
-from flask_themes2 import Themes
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-bootstrap = Bootstrap()
-themes = Themes()
 migrate = Migrate()
